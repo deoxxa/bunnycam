173a990233b04ca66e58a5cbe69db6568373f895
deoxxa <deoxxa@fknsrs.biz>
2015-07-12 17:47:49 +1000

    run streamer all the time, capturing one image per second

--- main.go
+++ main.go
@@ -7,38 +7,62 @@ import (
 	"os"
 	"os/exec"
 	"path"
+	"sort"
 	"time"
+
+	"gopkg.in/alecthomas/kingpin.v2"
 )
 
-func snapshot() (string, error) {
-	f := path.Join(os.TempDir(), fmt.Sprintf("%d.jpeg", time.Now().UnixNano()))
-
-	cmd := exec.Command("streamer", "-o", f)
-
-	if err := cmd.Run(); err != nil {
-		return f, err
-	}
-
-	return f, nil
-}
+var (
+	app            = kingpin.New("bunnycam", "Take pictures of a rabbit. Enjoy them.")
+	imageDirectory = app.Flag("images", "Where to read images from.").Default("/var/lib/bunnycam/data").OverrideDefaultFromEnvar("IMAGE_DIRECTORY").ExistingDir()
+	addr           = app.Flag("addr", "Address to listen on.").Default(":3000").OverrideDefaultFromEnvar("ADDR").String()
+)
 
 func main() {
+	kingpin.MustParse(app.Parse(os.Args[1:]))
+
+	go func() {
+		for {
+			exec.Command(
+				"streamer",
+				"-t", "99999999",
+				"-r", "1",
+				"-o", path.Join(*imageDirectory, fmt.Sprintf("snap_%d_00000000.jpeg", time.Now().Unix())),
+			).Run()
+
+			time.Sleep(time.Second)
+		}
+	}()
+
 	m := http.NewServeMux()
 
-	m.HandleFunc("/snapshot.jpeg", func(w http.ResponseWriter, r *http.Request) {
-		fn, err := snapshot()
+	m.HandleFunc("/latest.jpeg", func(w http.ResponseWriter, r *http.Request) {
+		d, err := os.Open(*imageDirectory)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		defer d.Close()
+
+		names, err := d.Readdirnames(0)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
 
-		f, err := os.Open(fn)
+		sort.Strings(names)
+
+		latest := names[len(names)-1]
+
+		f, err := os.Open(path.Join(*imageDirectory, latest))
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
 
 		w.Header().Set("content-type", "image/jpeg")
+		w.Header().Set("refresh", "1")
 
 		if _, err := io.Copy(w, f); err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -46,7 +70,7 @@ func main() {
 		}
 	})
 
-	if err := http.ListenAndServe(":3000", m); err != nil {
+	if err := http.ListenAndServe(*addr, m); err != nil {
 		panic(err)
 	}
 }
